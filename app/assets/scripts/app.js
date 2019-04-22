ca630f73a712ef705ffde7c87dc2a46cbd7c8c68
kostya3312 <kostya3312@gmail.com>
2019-04-23 00:09:24 +0300

    Complete modal functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 new MobileMenu();
+new Modal();
 
 new RevealOnScroll($(".feature-item"), "90%");
 new RevealOnScroll($(".testimonial"), "85%");
