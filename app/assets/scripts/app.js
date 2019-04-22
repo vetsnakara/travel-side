8b8d3728b1b9d91494240fe052ed3a5e046cd5ee
kostya3312 <kostya3312@gmail.com>
2019-04-22 22:13:06 +0300

    Complete sticky-header functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,11 @@
 import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 new MobileMenu();
+
 new RevealOnScroll($(".feature-item"), "90%");
 new RevealOnScroll($(".testimonial"), "85%");
+
+new StickyHeader();
